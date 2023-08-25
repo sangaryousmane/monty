f560204f7afe34124e82e5334e955f8d9681b0f3
sangaryousmane <sangaryousmane@gmail.com>
2023-08-25 03:41:48 -0700

    Fix and correct

--- _rotate.c
+++ _rotate.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 /**
   *_rotate- rotates the stack to the bottom
--- _rotate_top.c
+++ _rotate_top.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 /**
   *_rotate_top - rotates the stack to the top
