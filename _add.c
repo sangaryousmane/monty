2ac37804fd137838d148d58195748693d4664b25
sangaryousmane <sangaryousmane@gmail.com>
2023-08-27 04:47:56 -0700

    The opcode add adds the top two elements of the stack.

--- _add.c
+++ _add.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
+ * f_add - adds the stack's top two element.
+ * @head: the stack's head
+ * @counter: the line number
  * Return: no return
 */
 void f_add(stack_t **head, unsigned int counter)
